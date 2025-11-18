a2a4143dc6fa2d25cac9dadc0f140ad29674833f
欢乐马 <2487683083@qq.com>
2025-11-18 18:56:18 +0800

    Signed-off-by: 欢乐马 <2487683083@qq.com>

--- src/env-types.ts
+++
@@ -1,5 +0,0 @@
-export interface Env {
-    DB:D1Database;
-    DB1:D1Database;
-    DB2:D1Database;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
-import { Env } from './env-types';
+// import { Env } from './env-types';
 
 export default {
     async fetch(request: Request, env: Env): Promise<Response> {
         // DB=env.DB;
 
+        console.log("111");
         const url = new URL(request.url);
         const pathname = url.pathname;
-        return new Response('', { status: 404 });
+        // return new Response('', { status: 404 });
+        return new Response(JSON.stringify({ message: "Hello World" }), {
+  headers: { 'Content-Type': 'application/json' },
+});
     },
-
-
-// 错误响应辅助函数
-errorResponse(status: number, message: string): Response {
-    return new Response(JSON.stringify({ 
-        success: false, 
-        message: message 
-    }), { 
-        status: status,
-        headers: { 'Content-Type': 'application/json' }
-    });
-}
 };
